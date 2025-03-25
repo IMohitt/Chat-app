732eab99261a0fa14c5d28aca111d77d720952e3
Mohit Sharma <2204mohitsharma@gamil.com>
2025-03-25 15:00:27 +0530

    updated

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -7,7 +7,7 @@ import { getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBvktH3CPbM26mjLkcNNydILibmRmkj4Lk",
+  // apiKey: <your_api_key>,
   authDomain: "chatapp-28970.firebaseapp.com",
   projectId: "chatapp-28970",
   storageBucket: "chatapp-28970.appspot.com",
